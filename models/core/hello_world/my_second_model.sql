635d07638957abb2f66837782154f64e25d168f0
Randy <randy.pitcher@fishtownanalytics.com>
2021-10-13 14:33:48 +0000

    fixed runs

--- models/core/hello_world/my_second_model.sql
+++ models/core/hello_world/my_second_model.sql
@@ -2,8 +2,8 @@
 
 select 
     *,
-    created_at::date as today,
-    today - 1        as yesterday -- all my troubles seemed so far away
+    created_at::date   as today,
+    current_date() - 1 as yesterday -- all my troubles seemed so far away
 
 
 from {{ ref('my_first_model') }}
